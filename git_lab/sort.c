7fb98c3739f66c1fa261f3f1c564703db9f0a16c
Sasha <pastukhovaleksandr98@gmail.com>
2025-03-18 08:33:44 +0700

    Добавлен main() в sort.c

--- git_lab/sort.c
+++ git_lab/sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 void BubbleSort(int arr[], int n){
 	for (int i = 0; i < n-1; i++){
 		for (int j = 0; j < n - i -1; j++){
@@ -8,4 +10,16 @@ void BubbleSort(int arr[], int n){
 			}
 		}
 	}
+}
+
+int main(){
+	int a[] = {4,2,0};
+	int n = sizeof(a) / sizeof(a[0]);
+
+	sort(a,n);
+
+	for (int i=0, i < n; i++){
+		printf("%d ", a[i]);
+	}
+	return 0;
 }
